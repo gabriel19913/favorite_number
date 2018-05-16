9074bcc466a6e1b2e87c17ae35b2b4446b0be5f2
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2018-05-16 10:21:17 -0300

    Adding docstrings to the favorite_number.py file

--- favorite_number.py
+++ favorite_number.py
@@ -1,11 +1,13 @@
 import json
 def save_number():
+    '''Save the user favorite number into a json file.'''
     number = int(input('\nType your favorite number and a will save it for you: '))
     with open('favorite_number.json', 'w') as file_object:
         json.dump(number, file_object)
     return number
 
 def read_number():
+    '''Read the json file that contain the user favorite numver'''
     try:
         with open('favorite_number.json') as file_object:
             number = json.load(file_object)
@@ -15,6 +17,7 @@ def read_number():
         return number
 
 def show_number():
+    '''If the user aldeary informed a number it will show to him/her. Otherwise the function save_number will be called to ask the user his/her favorite number'''
     number = read_number()
     if number:
         print('\n------------------------------------------------------------')
@@ -26,5 +29,3 @@ def show_number():
         number = save_number()
         print('The number {} was saved as your favorite number.'.format(number))
         print('------------------------------------------------------------')
-
-show_number()
